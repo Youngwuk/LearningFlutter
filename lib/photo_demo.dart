f189984ccdba8ae48ccbf71a5d4aa6f2d392dc44
oimzi <oimzi@daum.net>
2019-12-03 18:35:15 +0900

    iu

--- lib/photo_demo.dart
+++ lib/photo_demo.dart
@@ -4,10 +4,53 @@ class PhotoDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Image.network(
-        'https://picsum.photos/250?image=9',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24.0, 48.0, 24.0, 0.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(24.0),
+                border: Border.all(color: Colors.red, width: 1,)
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(24.0),
+                child: Stack(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  children: <Widget>[
+                    Image.asset('assets/image/iu.jpg'),
+                    Container(
+                      width: double.infinity,
+                      height: 40,
+                      color: Colors.black54,
+                      child: Center(
+                          child: Text(
+                        'I.U',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 0.0),
+            child: Text(
+              '세계최고 여가수 아이유 by.토니',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontWeight: FontWeight.w800,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ],
       ),
     );
-
   }
 }
